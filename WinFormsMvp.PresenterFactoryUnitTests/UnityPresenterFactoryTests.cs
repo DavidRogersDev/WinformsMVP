128de197a59791088264b5b4a16991f0698139dd
David Rogers <davidrogersau@yahoo.com.au>
2013-12-31 16:23:15 +1030

    Added unit tests for Unity DI support.

---
+++ WinFormsMvp.PresenterFactoryUnitTests/UnityPresenterFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsMvp.Binder;
+using WinFormsMvp.Unity;
+
+namespace WinFormsMvp.PresenterFactoryUnitTests
+{
+    [TestClass]
+    public class UnityPresenterFactoryTests : PresenterFactoryTests
+    {
+        protected override IPresenterFactory BuildFactory()
+        {
+            var container = new UnityContainer();
+            return new UnityPresenterFactory(container);
+        }
+    }
+}
