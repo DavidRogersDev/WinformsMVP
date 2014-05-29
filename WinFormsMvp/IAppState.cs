844bcedfadcf0f63cc6bce0b66b18ef042d65adc
David Rogers <davidrogersau@yahoo.com.au>
2014-05-29 12:07:25 +0930

    Added further method to IAppState interface.

--- WinFormsMvp/IAppState.cs
+++ WinFormsMvp/IAppState.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace WinFormsMvp
 {
     public interface IAppState
     {
         void AddItem<T>(string key, T item);
+        void Clear();
         T GetItem<T>(string key);
+        bool HasItem(string key);
         void RemoveItem<T>(string key);
     }
 }
